4bc0cebab01af1c48b7f31f08b4831b032013468
Vijay Upadhyay <38240812+VijayUpadhyay@users.noreply.github.com>
2019-12-18 09:48:11 +0530

    included react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./page/homepage/home.component"
+import { Route } from 'react-router-dom'
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <switch>
+        <Route exact path='/hats' component={HatsPage} />
+        <Route exact='true' path='/' component={HomePage} />
+      </switch>
     </div>
   );
 }
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
 
-const MenuItem = ({ menutitle, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-
+const MenuItem = ({ title, imgUrl, size, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
+            backgroundImage: `url(${imgUrl})`
         }} />
         <div className='content'>
-            <h1 className='title'>{menutitle.toUpperCase()}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
